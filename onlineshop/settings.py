4eddf5d4f0e8e69bfd38e1d67d34d6d08b369677
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-07-06 14:58:44 +0300

    Change database to postgresql

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_shop',
+        'USER': 'online_shop_user',
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
