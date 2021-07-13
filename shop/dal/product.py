2cecfa0956c9bd4ee05bfe724821bd2c7caeeaf1
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-07-13 23:25:00 +0300

    Some refactoring related to mvc pattern

--- shop/dal/product.py
+++ shop/dal/product.py
@@ -1,17 +1,14 @@
-from django.http import Http404
-
 from shop.models import Product
 
 
 class ProductDAL:
     @classmethod
-    def get_all_or_category_products(cls, category=None, is_available=True):
-        return Product.objects.filter(is_available=is_available) if not category \
-            else Product.objects.filter(category=category, is_available=is_available)
+    def get_available_or_category_products(cls, category=None):
+        if category:
+            return Product.available_products.filter(category=category)
+        else:
+            return Product.available_products.all()
 
     @classmethod
     def get_product_by_pk(cls, product_pk, is_available=True):
-        try:
-            return Product.objects.get(pk=product_pk, is_available=is_available)
-        except Product.DoesNotExist:
-            raise Http404
+        return Product.objects.get(pk=product_pk, is_available=is_available)
