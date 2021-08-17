b433cfbea910075f6a2b0cfcc442e0f25d9b575e
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-08-17 13:50:35 +0300

    Move check_permissions decorator

--- shop/permissions.py
+++ shop/permissions.py
@@ -1,6 +1,19 @@
+from functools import wraps
+
 from rest_framework.permissions import BasePermission
 
 
 class IsOwnerOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user or request.user.is_staff
+
+
+def check_permissions(get_object_func):
+    def check_permissions_decorator(http_method):
+        @wraps(http_method)
+        def wrapper(self, *method_args, **method_kwargs):
+            request, pk = method_args[0], method_kwargs['pk']
+            self.check_object_permissions(request, get_object_func(pk))
+            return http_method(self, *method_args, **method_kwargs)
+        return wrapper
+    return check_permissions_decorator
