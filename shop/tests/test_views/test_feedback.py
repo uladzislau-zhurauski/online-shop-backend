3a4b083f8ed965127ff66c220dc19c4b9e41044f
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-08-24 16:29:34 +0300

    Write tests for deletion of specific feedback images

--- shop/tests/test_views/test_feedback.py
+++ shop/tests/test_views/test_feedback.py
@@ -1,10 +1,55 @@
+from enum import Enum, auto
+
 import pytest
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Count
 from django.urls import reverse
 from rest_framework import status
 
-from shop.models import Feedback
+from shop.models import Feedback, Image
 from shop.serializers.feedback import FeedbackListSerializer, FeedbackDetailSerializer
 from shop.tests.conftest import nonexistent_pk, ClientType, existent_pk, Arg
+
+
+class ImagesToDelete(Enum):
+    CORRECT_AND_ALL_PKS = auto()
+    CORRECT_AND_NOT_ALL_PKS = auto()
+    CORRECT_AND_INCORRECT = auto()
+    ALL_INCORRECT = auto()
+    EMPTY = auto()
+    WITHOUT_IMAGES = auto()
+
+
+@pytest.fixture
+def get_images_to_delete():
+    def _get_images_to_delete(data, moderated_feedback, variant):
+        if variant == ImagesToDelete.CORRECT_AND_ALL_PKS:
+            data['images_to_delete'] = [image.pk for image in moderated_feedback.images.all()]
+        elif variant == ImagesToDelete.CORRECT_AND_NOT_ALL_PKS:
+            data['images_to_delete'] = [image.pk for image in moderated_feedback.images.all()
+                                        if image != moderated_feedback.images.last()]
+        elif variant == ImagesToDelete.CORRECT_AND_INCORRECT:
+            images_to_delete = [image.pk for image in moderated_feedback.images.all()
+                                if image != moderated_feedback.images.last()]
+            # append image from another feedback
+            images_to_delete.append(Image.objects.exclude(object_id=moderated_feedback.pk)
+                                    .filter(content_type=ContentType.objects.get_for_model(Feedback)).first().pk)
+            data['images_to_delete'] = images_to_delete
+        elif variant == ImagesToDelete.ALL_INCORRECT:
+            data['images_to_delete'] = [nonexistent_pk]
+        elif variant == ImagesToDelete.EMPTY:
+            data['images_to_delete'] = []
+        elif variant == ImagesToDelete.WITHOUT_IMAGES:
+            data['images_to_delete'] = [existent_pk]
+        elif variant is None:
+            pass
+        else:
+            raise ValueError(f'Unhandled value: {variant} ({type(variant).__name__})')
+
+        print(f'\n{data.get("images_to_delete", "default")}\n')
+        return data
+
+    return _get_images_to_delete
 
 
 @pytest.mark.django_db
@@ -103,6 +148,34 @@ class TestFeedbackViews:
         user = moderated_feedback.author if client_type is ClientType.AUTHOR_CLIENT else None
         response = multi_client(client_type, user).put(url, data=data)
 
+        assert response.status_code == status_code
+
+    @pytest.mark.parametrize('has_images, images_to_delete, status_code', [
+        (True, None, status.HTTP_200_OK),
+        (True, ImagesToDelete.CORRECT_AND_ALL_PKS, status.HTTP_200_OK),
+        (True, ImagesToDelete.CORRECT_AND_NOT_ALL_PKS, status.HTTP_200_OK),
+        (True, ImagesToDelete.CORRECT_AND_INCORRECT, status.HTTP_400_BAD_REQUEST),
+        (True, ImagesToDelete.ALL_INCORRECT, status.HTTP_400_BAD_REQUEST),
+        (True, ImagesToDelete.EMPTY, status.HTTP_200_OK),
+        (False, ImagesToDelete.WITHOUT_IMAGES, status.HTTP_400_BAD_REQUEST),
+    ])
+    def test_feedback_put_with_images_to_delete(self, has_images, images_to_delete, status_code,
+                                                get_images_to_delete, authenticated_api_client):
+        if has_images:
+            moderated_feedback = Feedback.moderated_feedback.annotate(images_count=Count('images')). \
+                filter(images_count__gte=3).first()
+        else:
+            moderated_feedback = Feedback.moderated_feedback.filter(images__isnull=True).first()
+        url = reverse('feedback-detail', kwargs={'pk': moderated_feedback.pk})
+        data = {
+            'product': moderated_feedback.product.pk,
+            'title': moderated_feedback.title,
+            'content': moderated_feedback.content
+        }
+        data = get_images_to_delete(data, moderated_feedback, images_to_delete)
+        response = authenticated_api_client(is_admin=True).put(url, data=data)
+
+        print(response.data)
         assert response.status_code == status_code
 
     @pytest.mark.parametrize(
