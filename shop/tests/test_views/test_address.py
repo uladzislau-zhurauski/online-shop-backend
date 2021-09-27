3eb8a261ac0a281e39129962b05b9304176e7484
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-09-27 16:11:48 +0300

    Rewrite address view tests

--- shop/tests/test_views/test_address.py
+++ shop/tests/test_views/test_address.py
@@ -9,93 +9,140 @@ from shop.serializers.address import AddressOutputSerializer
 from shop.tests.conftest import ClientType, EXISTENT_PK, NONEXISTENT_PK
 
 
+@pytest.fixture
+def address_data(address_factory):
+    data = factory.build(dict, FACTORY_CLASS=address_factory)
+    data.pop('user')
+    return data
+
+
 @pytest.mark.django_db
 class TestAddressViews:
-    @pytest.mark.parametrize('client_type, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, status.HTTP_200_OK),
-        (ClientType.AUTHOR_CLIENT, status.HTTP_200_OK),
-        (ClientType.AUTH_CLIENT, status.HTTP_200_OK)
-    ])
-    def test_get_address_list(self, client_type, status_code, multi_client):
-        if client_type == ClientType.AUTHOR_CLIENT or client_type == ClientType.AUTH_CLIENT:
-            user = get_user_model().objects.filter(addresses__isnull=False).first()
-            address_list = user.addresses
-        else:
-            user = None
-            address_list = Address.objects.all()
+    def test_get_address_list_by_not_auth(self, api_client):
         url = reverse('address-list')
-        response = multi_client(client_type, user).get(url)
+        response = api_client.get(url)
 
-        assert response.status_code == status_code
-        if client_type != ClientType.NOT_AUTH_CLIENT:
-            assert response.data == AddressOutputSerializer(instance=address_list, many=True).data
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_get_address_with_nonexistent_pk(self, authenticated_api_client):
+    def test_get_address_list_by_auth_client(self, authenticated_api_client):
+        user = get_user_model().objects.filter(addresses__isnull=False).first()
+        address_list = user.addresses.all()
+        url = reverse('address-list')
+        response = authenticated_api_client(is_admin=False, user=user).get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == AddressOutputSerializer(instance=address_list, many=True).data
+
+    def test_get_address_list_by_admin(self, authenticated_api_client):
+        address_list = Address.objects.all()
+        url = reverse('address-list')
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == AddressOutputSerializer(instance=address_list, many=True).data
+
+    def test_get_nonexistent_address(self, authenticated_api_client):
         url = reverse('address-detail', kwargs={'pk': NONEXISTENT_PK})
         response = authenticated_api_client(is_admin=True).get(url)
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    @pytest.mark.parametrize('client_type, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, status.HTTP_200_OK),
-        (ClientType.AUTHOR_CLIENT, status.HTTP_200_OK),
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
     ])
-    def test_get_address(self, client_type, status_code, multi_client):
+    def test_forbidden_get_address(self, client_type, multi_client):
+        url = reverse('address-detail', kwargs={'pk': EXISTENT_PK})
+        response = multi_client(client_type).get(url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_get_address_by_author(self, authenticated_api_client):
         address = Address.objects.first()
         url = reverse('address-detail', kwargs={'pk': address.pk})
-        user = address.user if client_type == ClientType.AUTHOR_CLIENT else None
-        response = multi_client(client_type, user).get(url)
+        response = authenticated_api_client(is_admin=False, user=address.user).get(url)
 
-        assert response.status_code == status_code
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == AddressOutputSerializer(address).data
+
+    def test_get_address_by_admin(self, authenticated_api_client):
+        address = Address.objects.first()
+        url = reverse('address-detail', kwargs={'pk': address.pk})
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == AddressOutputSerializer(address).data
 
     @pytest.mark.parametrize('client_type, status_code', [
         (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
         (ClientType.AUTH_CLIENT, status.HTTP_201_CREATED),
         (ClientType.ADMIN_CLIENT, status.HTTP_201_CREATED)
     ])
-    def test_post_address(self, client_type, status_code, multi_client, address_factory):
-        data = factory.build(dict, FACTORY_CLASS=address_factory)
-        data.pop('user')
+    def test_post_address(self, client_type, status_code, multi_client, address_data):
+        data = address_data
         url = reverse('address-list')
-        response = multi_client(client_type, None).post(url, data=data)
+        response = multi_client(client_type).post(url, data=data)
 
         assert response.status_code == status_code
 
-    @pytest.mark.parametrize('client_type, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTHOR_CLIENT, status.HTTP_200_OK),
-        (ClientType.ADMIN_CLIENT, status.HTTP_200_OK),
+    def test_put_nonexistent_address(self, authenticated_api_client, address_data):
+        data = address_data
+        url = reverse('address-detail', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).put(url, data=data)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
     ])
-    def test_put_address(self, client_type, status_code, multi_client, address_factory):
-        data = factory.build(dict, FACTORY_CLASS=address_factory)
-        data.pop('user')
+    def test_forbidden_put_address(self, client_type, multi_client, address_data):
+        data = address_data
+        url = reverse('address-detail', kwargs={'pk': EXISTENT_PK})
+        response = multi_client(client_type).put(url, data=data)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_put_address_by_author(self, authenticated_api_client, address_data):
+        data = address_data
         address = Address.objects.first()
         url = reverse('address-detail', kwargs={'pk': address.pk})
-        user = address.user if client_type == ClientType.AUTHOR_CLIENT else None
-        response = multi_client(client_type, user).put(url, data=data)
+        response = authenticated_api_client(is_admin=False, user=address.user).put(url, data=data)
 
-        assert response.status_code == status_code
+        assert response.status_code == status.HTTP_200_OK
 
-    @pytest.mark.parametrize(
-        'client_type, address_pk, status_code', [
-            (ClientType.NOT_AUTH_CLIENT, EXISTENT_PK, status.HTTP_403_FORBIDDEN),
-            (ClientType.AUTH_CLIENT, EXISTENT_PK, status.HTTP_403_FORBIDDEN),
-            (ClientType.ADMIN_CLIENT, NONEXISTENT_PK, status.HTTP_404_NOT_FOUND),
-            (ClientType.ADMIN_CLIENT, EXISTENT_PK, status.HTTP_204_NO_CONTENT),
-            (ClientType.AUTHOR_CLIENT, NONEXISTENT_PK, status.HTTP_404_NOT_FOUND),
-            (ClientType.AUTHOR_CLIENT, EXISTENT_PK, status.HTTP_204_NO_CONTENT)
-        ]
-    )
-    def test_delete_address(self, client_type, status_code, address_pk, multi_client):
-        url = reverse('address-detail', kwargs={'pk': address_pk})
-        if client_type == ClientType.AUTHOR_CLIENT and address_pk == EXISTENT_PK:
-            user = Address.objects.get(pk=address_pk).user
-        else:
-            user = None
-        response = multi_client(client_type, user).delete(url)
+    def test_put_address_by_admin(self, authenticated_api_client, address_data):
+        data = address_data
+        url = reverse('address-detail', kwargs={'pk': EXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).put(url, data=data)
 
-        assert response.status_code == status_code
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_delete_nonexistent_address(self, authenticated_api_client):
+        url = reverse('address-detail', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).delete(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
+    ])
+    def test_forbidden_delete_existent_address(self, client_type, multi_client):
+        url = reverse('address-detail', kwargs={'pk': EXISTENT_PK})
+        response = multi_client(client_type).delete(url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_delete_existent_address_by_author(self, authenticated_api_client):
+        address = Address.objects.first()
+        url = reverse('address-detail', kwargs={'pk': address.pk})
+        response = authenticated_api_client(is_admin=False, user=address.user).delete(url)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_delete_existent_address_by_admin(self, authenticated_api_client):
+        url = reverse('address-detail', kwargs={'pk': EXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).delete(url)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
