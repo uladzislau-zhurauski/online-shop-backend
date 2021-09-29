32e8380575e16439fe493028abf50166a4484a81
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-09-29 11:49:30 +0300

    Rewrite product view tests

--- shop/tests/test_views/test_product.py
+++ shop/tests/test_views/test_product.py
@@ -10,20 +10,56 @@ from shop.serializers.product import ProductOutputSerializer
 from shop.tests.conftest import Arg, ClientType, EXISTENT_MATERIAL_NAME, EXISTENT_PK, NONEXISTENT_PK
 
 
+@pytest.fixture
+def product_data(product_factory, get_in_memory_image_file, product_images_to_delete):
+    def _product_data(materials=None, images_to_delete=None, product_obj=None):
+        data = factory.build(dict, FACTORY_CLASS=product_factory)
+        data['category'] = EXISTENT_PK
+        if materials is None:
+            data['materials'] = [EXISTENT_MATERIAL_NAME, 'nonexistent material 1', 'nonexistent material 2']
+        else:
+            data['materials'] = materials
+        data['images'] = [get_in_memory_image_file]
+        if images_to_delete is None:
+            data['images_to_delete'] = [1, 2, 3]
+        else:
+            data['images_to_delete'] = product_images_to_delete(images_to_delete, product_obj)
+        return data
+    return _product_data
+
+
+@pytest.fixture
+def product_images_to_delete():
+    def _product_images_to_delete(images_to_delete, product_obj):
+        if images_to_delete == Arg.CORRECT:
+            if product_obj is None:
+                raise ValueError('product_obj must be set if images_to_delete is Arg.Correct')
+            return [product_obj.images.first().pk]
+        elif images_to_delete == Arg.INCORRECT:
+            return [NONEXISTENT_PK]
+        else:
+            raise UnhandledValueError(images_to_delete)
+    return _product_images_to_delete
+
+
 @pytest.mark.django_db
 class TestProductViews:
-    @pytest.mark.parametrize('client_type',
-                             [ClientType.NOT_AUTH_CLIENT, ClientType.AUTH_CLIENT, ClientType.ADMIN_CLIENT])
-    def test_get_product_list(self, client_type, multi_client):
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
+    ])
+    def test_get_product_list_by_ordinary_users(self, client_type, multi_client):
         url = reverse('product-list')
         response = multi_client(client_type).get(url)
+        products = Product.available_products.all()
 
-        if client_type in [ClientType.NOT_AUTH_CLIENT, ClientType.AUTH_CLIENT]:
-            products = Product.available_products.all()
-        elif client_type == ClientType.ADMIN_CLIENT:
-            products = Product.objects.all()
-        else:
-            raise UnhandledValueError(client_type)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == ProductOutputSerializer(products, many=True).data
+
+    def test_get_product_list_by_admin(self, authenticated_api_client):
+        url = reverse('product-list')
+        response = authenticated_api_client(is_admin=True).get(url)
+        products = Product.objects.all()
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data == ProductOutputSerializer(products, many=True).data
@@ -36,69 +72,92 @@ class TestProductViews:
         assert response.status_code == status.HTTP_200_OK
         assert response.data == []
 
-    @pytest.mark.parametrize('client_type',
-                             [ClientType.NOT_AUTH_CLIENT, ClientType.AUTH_CLIENT, ClientType.ADMIN_CLIENT])
-    def test_get_existing_product_list_by_category(self, client_type, multi_client):
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
+    ])
+    def test_get_existing_product_list_by_category_by_ordinary_users(self, client_type, multi_client):
         multiple_products_category = Category.objects.annotate(products_count=Count('products')). \
             filter(products_count__gt=1).first()
-        if client_type in [ClientType.NOT_AUTH_CLIENT, ClientType.AUTH_CLIENT]:
-            products = Product.available_products.filter(category=multiple_products_category)
-        elif client_type == ClientType.ADMIN_CLIENT:
-            products = Product.objects.filter(category=multiple_products_category)
-        else:
-            raise UnhandledValueError(client_type)
+        products = Product.available_products.filter(category=multiple_products_category)
         url = reverse('product-list-by-category', kwargs={'category_pk': multiple_products_category.pk})
         response = multi_client(client_type).get(url)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data == ProductOutputSerializer(products, many=True).data
 
-    @pytest.mark.parametrize('client_type',
-                             [ClientType.NOT_AUTH_CLIENT, ClientType.AUTH_CLIENT, ClientType.ADMIN_CLIENT])
-    def test_get_product_with_nonexistent_pk(self, client_type, multi_client):
+    def test_get_existing_product_list_by_category_by_admin(self, authenticated_api_client):
+        multiple_products_category = Category.objects.annotate(products_count=Count('products')). \
+            filter(products_count__gt=1).first()
+        products = Product.objects.filter(category=multiple_products_category)
+        url = reverse('product-list-by-category', kwargs={'category_pk': multiple_products_category.pk})
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == ProductOutputSerializer(products, many=True).data
+
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT,
+        ClientType.ADMIN_CLIENT
+    ])
+    def test_get_nonexistent_product(self, client_type, multi_client):
         url = reverse('product-detail', kwargs={'pk': NONEXISTENT_PK})
         response = multi_client(client_type).get(url)
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    @pytest.mark.parametrize('client_type, status_code',
-                             [(ClientType.NOT_AUTH_CLIENT, status.HTTP_404_NOT_FOUND),
-                              (ClientType.AUTH_CLIENT, status.HTTP_404_NOT_FOUND),
-                              (ClientType.ADMIN_CLIENT, status.HTTP_200_OK)])
-    def test_get_unavailable_product(self, client_type, status_code, multi_client):
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
+    ])
+    def test_get_unavailable_product_by_ordinary_users(self, client_type, multi_client):
         unavailable_product = Product.objects.filter(is_available=False).first()
         url = reverse('product-detail', kwargs={'pk': unavailable_product.pk})
         response = multi_client(client_type).get(url)
 
-        assert response.status_code == status_code
-        if client_type == ClientType.ADMIN_CLIENT:
-            assert response.data == ProductOutputSerializer(unavailable_product).data
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    @pytest.mark.parametrize('client_type',
-                             [ClientType.NOT_AUTH_CLIENT, ClientType.AUTH_CLIENT, ClientType.ADMIN_CLIENT])
+    def test_get_unavailable_product_by_admin(self, authenticated_api_client):
+        unavailable_product = Product.objects.filter(is_available=False).first()
+        url = reverse('product-detail', kwargs={'pk': unavailable_product.pk})
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == ProductOutputSerializer(unavailable_product).data
+
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT,
+        ClientType.ADMIN_CLIENT
+    ])
     def test_get_available_product(self, client_type, multi_client):
-        product = Product.available_products.first()
-        url = reverse('product-detail', kwargs={'pk': product.pk})
+        available_product = Product.available_products.first()
+        url = reverse('product-detail', kwargs={'pk': available_product.pk})
         response = multi_client(client_type).get(url)
 
         assert response.status_code == status.HTTP_200_OK
-        assert response.data == ProductOutputSerializer(instance=product).data
+        assert response.data == ProductOutputSerializer(instance=available_product).data
 
     @pytest.mark.parametrize('client_type, status_code', [
         (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
         (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
         (ClientType.ADMIN_CLIENT, status.HTTP_201_CREATED),
     ])
-    def test_post_product(self, client_type, status_code, multi_client, product_factory, get_in_memory_image_file):
-        data = factory.build(dict, FACTORY_CLASS=product_factory)
-        data['category'] = EXISTENT_PK
-        data['materials'] = [EXISTENT_MATERIAL_NAME, 'material 2', 'material 3']
-        data['images'] = [get_in_memory_image_file]
-        data['images_to_delete'] = [1, 2, 3]
+    def test_post_product(self, client_type, status_code, multi_client, product_data):
+        data = product_data()
         url = reverse('product-list')
         response = multi_client(client_type).post(url, data=data)
 
         assert response.status_code == status_code
+
+    def test_put_nonexistent_product(self, product_data, authenticated_api_client):
+        product_to_update = Product.objects.first()
+        data = product_data([], Arg.CORRECT, product_to_update)
+        url = reverse('product-detail', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).put(url, data=data)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
     @pytest.mark.parametrize('client_type, materials, images_to_delete, status_code', [
         (ClientType.NOT_AUTH_CLIENT, [], Arg.CORRECT, status.HTTP_403_FORBIDDEN),
@@ -108,43 +167,30 @@ class TestProductViews:
          status.HTTP_200_OK),
         (ClientType.ADMIN_CLIENT, [], Arg.INCORRECT, status.HTTP_400_BAD_REQUEST)
     ])
-    def test_put_product(self, client_type, materials, images_to_delete, status_code, multi_client, product_factory,
-                         get_in_memory_image_file):
+    def test_put_product(self, client_type, materials, images_to_delete, status_code, multi_client, product_data):
         product_to_update = Product.objects.first()
-        data = factory.build(dict, FACTORY_CLASS=product_factory)
-        data['category'] = EXISTENT_PK
-        data['materials'] = materials
-        data['images'] = [get_in_memory_image_file]
-        if images_to_delete == Arg.CORRECT:
-            data['images_to_delete'] = [product_to_update.images.first().pk]
-        elif images_to_delete == Arg.INCORRECT:
-            data['images_to_delete'] = [NONEXISTENT_PK]
-        else:
-            raise UnhandledValueError(images_to_delete)
+        data = product_data(materials, images_to_delete, product_to_update)
         url = reverse('product-detail', kwargs={'pk': product_to_update.pk})
         response = multi_client(client_type).put(url, data=data)
 
         assert response.status_code == status_code
-        if status_code == status.HTTP_400_BAD_REQUEST:
-            assert 'images_to_delete' in response.data
 
-    @pytest.mark.parametrize('client_type, status_code',
-                             [(ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-                              (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-                              (ClientType.ADMIN_CLIENT, status.HTTP_404_NOT_FOUND)])
-    def test_delete_product_with_nonexistent_pk(self, client_type, status_code, multi_client):
+    @pytest.mark.parametrize('client_type, status_code', [
+        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.ADMIN_CLIENT, status.HTTP_404_NOT_FOUND)
+    ])
+    def test_delete_nonexistent_product(self, client_type, status_code, multi_client):
         url = reverse('product-detail', kwargs={'pk': NONEXISTENT_PK})
         response = multi_client(client_type).delete(url)
 
         assert response.status_code == status_code
 
-    @pytest.mark.parametrize(
-        'client_type, status_code', [
-            (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-            (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-            (ClientType.ADMIN_CLIENT, status.HTTP_204_NO_CONTENT)
-        ]
-    )
+    @pytest.mark.parametrize('client_type, status_code', [
+        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.ADMIN_CLIENT, status.HTTP_204_NO_CONTENT)
+    ])
     def test_delete_product(self, client_type, status_code, multi_client):
         product = Product.objects.first()
         url = reverse('product-detail', kwargs={'pk': product.pk})
@@ -154,13 +200,17 @@ class TestProductViews:
 
 
 class TestProductImagesRemover:
-    @pytest.mark.parametrize(
-        'client_type, status_code', [
-            (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-            (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-            (ClientType.ADMIN_CLIENT, status.HTTP_200_OK)
-        ]
-    )
+    def test_delete_nonexistent_product_images(self, authenticated_api_client):
+        url = reverse('product-detail-delete-images', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize('client_type, status_code', [
+        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.ADMIN_CLIENT, status.HTTP_200_OK)
+    ])
     @pytest.mark.parametrize('has_images', [True, False])
     def test_delete_product_images(self, client_type, status_code, has_images, multi_client):
         product = Product.objects.filter(images__isnull=has_images).distinct().first()
