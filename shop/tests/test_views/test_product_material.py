1535c1ea1d9a28980cd95835c6e57bfbd35bab02
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-09-29 13:40:17 +0300

    Rewrite material view tests

--- shop/tests/test_views/test_product_material.py
+++ shop/tests/test_views/test_product_material.py
@@ -1,100 +1,153 @@
+from enum import Enum, auto
+
 import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from shop.exceptions import UnhandledValueError
 from shop.models import Product, ProductMaterial
 from shop.serializers.product_material import MaterialOutputSerializer
 from shop.tests.conftest import ClientType, EXISTENT_MATERIAL_NAME, EXISTENT_PK, NONEXISTENT_MATERIAL_NAME, \
     NONEXISTENT_PK
 
 
+class MaterialName(Enum):
+    UNIQUE = auto()
+    NOT_UNIQUE = auto()
+    THE_SAME = auto()
+
+
+@pytest.fixture
+def material_data(material_name_for_post, material_name_for_put):
+    def _material_data(name=MaterialName.UNIQUE, current_name=None):
+        data = {'products': [product.pk for product in Product.objects.all()[:5]]}
+        if current_name is None:
+            data['name'] = material_name_for_post(name)
+        else:
+            data['name'] = material_name_for_put(name, current_name)
+        return data
+    return _material_data
+
+
+@pytest.fixture
+def material_name_for_post():
+    def _material_name_for_post(name):
+        if name == MaterialName.UNIQUE:
+            return NONEXISTENT_MATERIAL_NAME
+        elif name == MaterialName.NOT_UNIQUE:  # create material with someone else's name
+            return EXISTENT_MATERIAL_NAME
+        else:
+            raise UnhandledValueError(name)
+    return _material_name_for_post
+
+
+@pytest.fixture
+def material_name_for_put():
+    def _material_name_for_put(name, current_name):
+        if name == MaterialName.UNIQUE:
+            return NONEXISTENT_MATERIAL_NAME
+        elif name == MaterialName.NOT_UNIQUE:  # update material with someone else's name
+            # We need to make sure the generated name != current name
+            return ProductMaterial.objects.exclude(name=current_name).first().name
+        elif name == MaterialName.THE_SAME:  # update material with its name
+            return current_name
+        else:
+            raise UnhandledValueError(name)
+    return _material_name_for_put
+
+
 @pytest.mark.django_db
 class TestProductMaterialViews:
-    @pytest.mark.parametrize('client_type, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, status.HTTP_200_OK)
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
     ])
-    def test_get_material_list(self, client_type, status_code, multi_client):
+    def test_forbidden_get_material_list(self, client_type, multi_client):
         url = reverse('material-list')
         response = multi_client(client_type).get(url)
 
-        assert response.status_code == status_code
-        if client_type == ClientType.ADMIN_CLIENT:
-            material_list = ProductMaterial.objects.all()
-            assert response.data == MaterialOutputSerializer(instance=material_list, many=True).data
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    @pytest.mark.parametrize('client_type, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, status.HTTP_404_NOT_FOUND)
-    ])
-    def test_get_material_with_nonexistent_pk(self, client_type, status_code, multi_client):
+    def test_get_material_list_by_admin(self, authenticated_api_client):
+        material_list = ProductMaterial.objects.all()
+        url = reverse('material-list')
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == MaterialOutputSerializer(instance=material_list, many=True).data
+
+    def test_get_nonexistent_material(self, authenticated_api_client):
         url = reverse('material-detail', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).get(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize('client_type', [
+        ClientType.NOT_AUTH_CLIENT,
+        ClientType.AUTH_CLIENT
+    ])
+    def test_forbidden_get_existent_material(self, client_type, multi_client):
+        url = reverse('material-detail', kwargs={'pk': EXISTENT_PK})
         response = multi_client(client_type).get(url)
 
-        assert response.status_code == status_code
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    @pytest.mark.parametrize('client_type, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, status.HTTP_200_OK)
-    ])
-    def test_get_material(self, client_type, status_code, multi_client):
+    def test_get_existent_material_by_admin(self, authenticated_api_client):
         material = ProductMaterial.objects.first()
         url = reverse('material-detail', kwargs={'pk': material.pk})
-        response = multi_client(client_type).get(url)
+        response = authenticated_api_client(is_admin=True).get(url)
 
-        assert response.status_code == status_code
-        if client_type == ClientType.ADMIN_CLIENT:
-            assert response.data == MaterialOutputSerializer(instance=material).data
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == MaterialOutputSerializer(instance=material).data
 
-    @pytest.mark.parametrize('client_type, name, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, NONEXISTENT_MATERIAL_NAME, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, NONEXISTENT_MATERIAL_NAME, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, EXISTENT_MATERIAL_NAME, status.HTTP_400_BAD_REQUEST),
-        (ClientType.ADMIN_CLIENT, NONEXISTENT_MATERIAL_NAME, status.HTTP_201_CREATED)
+    @pytest.mark.parametrize('client_type, name_type, status_code', [
+        (ClientType.NOT_AUTH_CLIENT, MaterialName.UNIQUE, status.HTTP_403_FORBIDDEN),
+        (ClientType.AUTH_CLIENT, MaterialName.UNIQUE, status.HTTP_403_FORBIDDEN),
+        (ClientType.ADMIN_CLIENT, MaterialName.NOT_UNIQUE, status.HTTP_400_BAD_REQUEST),
+        (ClientType.ADMIN_CLIENT, MaterialName.UNIQUE, status.HTTP_201_CREATED)
     ])
-    def test_post_material(self, client_type, name, status_code, multi_client):
-        data = {'name': name, 'products': [product.pk for product in Product.objects.all()[:5]]}
+    def test_post_material(self, client_type, name_type, status_code, material_data, multi_client):
+        data = material_data(name_type)
         url = reverse('material-list')
         response = multi_client(client_type).post(url, data=data)
 
         assert response.status_code == status_code
 
-    @pytest.mark.parametrize('client_type, name, status_code', [
-        (ClientType.NOT_AUTH_CLIENT, NONEXISTENT_MATERIAL_NAME, status.HTTP_403_FORBIDDEN),
-        (ClientType.AUTH_CLIENT, NONEXISTENT_MATERIAL_NAME, status.HTTP_403_FORBIDDEN),
-        (ClientType.ADMIN_CLIENT, EXISTENT_MATERIAL_NAME, status.HTTP_400_BAD_REQUEST),
-        (ClientType.ADMIN_CLIENT, EXISTENT_MATERIAL_NAME, status.HTTP_200_OK),
-        (ClientType.ADMIN_CLIENT, NONEXISTENT_MATERIAL_NAME, status.HTTP_200_OK)
-    ])
-    def test_put_material(self, client_type, name, status_code, multi_client):
-        data = {'products': [product.pk for product in Product.objects.all()[:5]]}
-        material = ProductMaterial.objects.first()
-        if name == NONEXISTENT_MATERIAL_NAME:
-            data['name'] = NONEXISTENT_MATERIAL_NAME
-        else:
-            if status_code == status.HTTP_400_BAD_REQUEST:  # update material with someone else's name must be an error
-                data['name'] = ProductMaterial.objects.exclude(name=name).first().name
-            else:
-                data['name'] = material.name  # update material with its name should be ok
+    def test_put_nonexistent_material(self, material_data, authenticated_api_client):
+        data = material_data()
+        url = reverse('material-detail', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).put(url, data=data)
 
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize('client_type, name_type, status_code', [
+        (ClientType.NOT_AUTH_CLIENT, MaterialName.UNIQUE, status.HTTP_403_FORBIDDEN),
+        (ClientType.AUTH_CLIENT, MaterialName.UNIQUE, status.HTTP_403_FORBIDDEN),
+        (ClientType.ADMIN_CLIENT, MaterialName.NOT_UNIQUE, status.HTTP_400_BAD_REQUEST),
+        (ClientType.ADMIN_CLIENT, MaterialName.THE_SAME, status.HTTP_200_OK),
+        (ClientType.ADMIN_CLIENT, MaterialName.UNIQUE, status.HTTP_200_OK)
+    ])
+    def test_put_existent_material(self, client_type, name_type, status_code, material_data, multi_client):
+        material = ProductMaterial.objects.first()
+        data = material_data(name_type, material.name)
         url = reverse('material-detail', kwargs={'pk': material.pk})
         response = multi_client(client_type).put(url, data=data)
 
         assert response.status_code == status_code
 
-    @pytest.mark.parametrize(
-        'client_type, material_pk, status_code', [
-            (ClientType.NOT_AUTH_CLIENT, EXISTENT_PK, status.HTTP_403_FORBIDDEN),
-            (ClientType.AUTH_CLIENT, EXISTENT_PK, status.HTTP_403_FORBIDDEN),
-            (ClientType.ADMIN_CLIENT, NONEXISTENT_PK, status.HTTP_404_NOT_FOUND),
-            (ClientType.ADMIN_CLIENT, EXISTENT_PK, status.HTTP_204_NO_CONTENT)
-        ]
-    )
-    def test_delete_material(self, client_type, status_code, material_pk, multi_client):
-        url = reverse('material-detail', kwargs={'pk': material_pk})
+    def test_delete_nonexistent_material(self, authenticated_api_client):
+        url = reverse('material-detail', kwargs={'pk': NONEXISTENT_PK})
+        response = authenticated_api_client(is_admin=True).delete(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize('client_type, status_code', [
+        (ClientType.NOT_AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.AUTH_CLIENT, status.HTTP_403_FORBIDDEN),
+        (ClientType.ADMIN_CLIENT, status.HTTP_204_NO_CONTENT)
+    ])
+    def test_delete_existent_material(self, client_type, status_code, multi_client):
+        url = reverse('material-detail', kwargs={'pk': EXISTENT_PK})
         response = multi_client(client_type).delete(url)
 
         assert response.status_code == status_code
